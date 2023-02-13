1bc16cc6d786ac494ffd90136989826f043c7856
gressho <werner.gresshoff@uni-muenster.de>
2023-02-13 17:46:48 +0100

    distribute!

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Development Status :: 1 - Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
